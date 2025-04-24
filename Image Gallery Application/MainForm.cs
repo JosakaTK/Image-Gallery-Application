bc1d50657aab27b45a1fc7b51566102dc5943e75
JosakaTK <90118195+JosakaTK@users.noreply.github.com>
2025-04-24 14:21:04 -0400

    Form changes

---
+++ Image Gallery Application/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Gallery_Application
+{
+    public partial class MainForm : Form
+    {
+        public Dictionary<string, PhotoInfo> photoList = new Dictionary<string, PhotoInfo>();
+        private List<Image> imageName = new List<Image>();
+        private string[] fileType = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+
+        public void getPhotoData()
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                if (openFileDialog.FileName.ToLower().Contains(fileType[0]) ||
+                    openFileDialog.FileName.ToLower().Contains(fileType[1]) ||
+                    openFileDialog.FileName.ToLower().Contains(fileType[2]) ||
+                    openFileDialog.FileName.ToLower().Contains(fileType[3]) ||
+                    openFileDialog.FileName.ToLower().Contains(fileType[4]))
+                {
+                    string fileName = Path.GetFileName(openFileDialog.FileName);
+                    string date = DateTime.Now.ToString("yyyy-MM-dd");
+                    PhotoInfo photoInfo = new PhotoInfo(fileName, date);
+                    photoList.Add(setTag(),photoInfo);
+                    imageName.Add(Image.FromFile(openFileDialog.FileName)); 
+                    lbImages.Items.Add(photoList.Last().Key);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a valid image file.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No file selected.");
+            }
+        }
+
+        public string setTag()
+        {
+            var tagForm = new TagForm();
+            tagForm.ShowDialog();
+            return tagForm.tbTag.Text;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            getPhotoData();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lbImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            gbImageTitle.Text = lbImages.SelectedItem.ToString();
+            pbImage.Image = imageName[lbImages.SelectedIndex];
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            int index = lbImages.SelectedIndex;
+            var displayForm = new ImageInfo();
+
+            displayForm.lblTags.Text = photoList.ElementAt(index).Key;
+            displayForm.lblDate.Text = photoList.ElementAt(index).Value.Date;
+            displayForm.lblFileName.Text = photoList.ElementAt(index).Value.FileName;
+
+            displayForm.ShowDialog();
+        }
+    }
+}
--- Image Gallery Application/Program.cs
+++ Image Gallery Application/Program.cs
@@ -16,7 +16,7 @@ namespace Image_Gallery_Application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
