836d49ad680ba2b7ca4e94c4b4ff9205855a9e91
JosakaTK <90118195+JosakaTK@users.noreply.github.com>
2025-04-24 14:21:04 -0400

    Form changes

---
+++ Image Gallery Application/ImageInfo.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Image_Gallery_Application
+{
+    partial class ImageInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTags = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTags);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblDate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lblFileName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 124);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image Data";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblTags
+            // 
+            this.lblTags.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTags.Location = new System.Drawing.Point(94, 84);
+            this.lblTags.Name = "lblTags";
+            this.lblTags.Size = new System.Drawing.Size(317, 20);
+            this.lblTags.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(54, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tags:";
+            // 
+            // lblDate
+            // 
+            this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDate.Location = new System.Drawing.Point(94, 55);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(317, 20);
+            this.lblDate.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date Uploaded:";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFileName.Location = new System.Drawing.Point(94, 26);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(317, 20);
+            this.lblFileName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Name:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(183, 142);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // ImageInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 178);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ImageInfo";
+            this.Text = "ImageInfo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClose;
+        public System.Windows.Forms.Label lblDate;
+        public System.Windows.Forms.Label lblFileName;
+        public System.Windows.Forms.Label lblTags;
+    }
+}
---
+++ Image Gallery Application/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Image_Gallery_Application
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbImageTitle = new System.Windows.Forms.GroupBox();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnOrganize = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbImages = new System.Windows.Forms.ListBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.gbImageTitle.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Image Gallery";
+            // 
+            // gbImageTitle
+            // 
+            this.gbImageTitle.Controls.Add(this.pbImage);
+            this.gbImageTitle.Location = new System.Drawing.Point(15, 43);
+            this.gbImageTitle.Name = "gbImageTitle";
+            this.gbImageTitle.Size = new System.Drawing.Size(411, 284);
+            this.gbImageTitle.TabIndex = 2;
+            this.gbImageTitle.TabStop = false;
+            this.gbImageTitle.Text = "Blank";
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(172, 349);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(97, 42);
+            this.btnView.TabIndex = 0;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(69, 349);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(97, 42);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnOrganize
+            // 
+            this.btnOrganize.Location = new System.Drawing.Point(275, 349);
+            this.btnOrganize.Name = "btnOrganize";
+            this.btnOrganize.Size = new System.Drawing.Size(97, 42);
+            this.btnOrganize.TabIndex = 4;
+            this.btnOrganize.Text = "Organize";
+            this.btnOrganize.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(475, 359);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbImages);
+            this.groupBox1.Location = new System.Drawing.Point(432, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 284);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image List";
+            // 
+            // lbImages
+            // 
+            this.lbImages.FormattingEnabled = true;
+            this.lbImages.Location = new System.Drawing.Point(6, 19);
+            this.lbImages.Name = "lbImages";
+            this.lbImages.Size = new System.Drawing.Size(178, 251);
+            this.lbImages.TabIndex = 0;
+            this.lbImages.SelectedIndexChanged += new System.EventHandler(this.lbImages_SelectedIndexChanged);
+            // 
+            // pbImage
+            // 
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Enabled = false;
+            this.pbImage.Location = new System.Drawing.Point(6, 19);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(399, 251);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 7;
+            this.pbImage.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 415);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOrganize);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.gbImageTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Image Gallery Application";
+            this.gbImageTitle.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbImageTitle;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnOrganize;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbImages;
+        private System.Windows.Forms.PictureBox pbImage;
+    }
+}
+
